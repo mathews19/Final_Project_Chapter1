42aed03c9f3fe646870364c85338fe64c545bc22
mathews19 <48871936+mathews19@users.noreply.github.com>
2021-07-20 20:54:03 -0300

    Entity Client

---
+++ src/main/java/com/devsuperior/cap1/entities/Client.java
@@ -0,0 +1,66 @@
+package com.devsuperior.cap1.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Double getIncome() {
+		return income;
+	}
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+	public Integer getChildren() {
+		return children;
+	}
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	
+	
+	
+	
+
+}
