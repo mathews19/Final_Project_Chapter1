ef43dc6e0199559ec2ef69179d3bfad649c37bd2
mathews19 <48871936+mathews19@users.noreply.github.com>
2021-07-20 20:05:38 -0300

    Inicio do repositório do projeto final

---
+++ src/main/java/com/devsuperior/cap1/Cap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.cap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/cap1/Cap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.cap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
